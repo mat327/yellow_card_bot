3a1232a48d4c8d5e8471283a3b00acfe454dda4c
Bialy <onetuchidol@gmail.com>
2022-10-05 10:31:21 +0200

    v0.5.4
    Repaired firebase backup

--- firebase_backup.py
+++ firebase_backup.py
@@ -1,26 +1,29 @@
+import os
 import time
 from tkinter import *
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-def make_backup_firebase(msg_id, user_id, ban_duration, terminal):
-    cred = credentials.Certificate("firebase_key.json")
-    firebase_admin.initialize_app(cred)
-    db=firestore.client()
+cred = credentials.Certificate("firebase_key.json")
+firebase_admin.initialize_app(cred)
+db=firestore.client()
 
+def make_backup_firebase(msg_id, user_id, ban_duration, terminal):
     #dodanie nowej wiadomosci do backupu
     data = {'msg_id': int(msg_id)}
     db.collection('banned_messages').add(data)
 
-    # Update data with unknown key: second way
-    doc = db.collection('banned_users_stats').where("user_id", "==", int(user_id)).get() # Get all documents with age >=40
-    if doc.exists:
-        key = doc.id
-        db.collection('banned_users_stats').document(key).update({"number_of_bans": firestore.Increment(1), "bans_time": firestore.Increment(ban_duration)}) # increment a field
-    else :
-        data = {'user_id': int(user_id), 'number_of_bans': 1, 'bans_time': ban_duration}
+    #aktualizacja statystyk banow
+    docs = db.collection(u'banned_users_stats').where('user_id', '==', str(user_id)).get()
+
+    if not docs:
+        data = {'user_id': str(user_id), 'number_of_bans': 1, 'bans_time': ban_duration}
         db.collection('banned_users_stats').add(data)
+    else :
+        for doc in docs:
+            key = doc.id
+            db.collection('banned_users_stats').document(key).update({'number_of_bans': firestore.Increment(1), 'bans_time': firestore.Increment(ban_duration)}) # increment a fields
 
     sec = time.localtime() # get struct_time
     terminal.insert(END, time.strftime("%d/%m/%Y, %H:%M:%S", sec) + "   Firebase Backup updated.")
